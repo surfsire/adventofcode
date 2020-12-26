2eb79f2d58069d5f405dcf94fe935262af24c090
MarkZ <surfsire@gmail.com>
2020-12-26 14:43:31 -0800

    Initial

---
+++ 2020/Day03/Day03.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+#open file
+print("Opening File...")
+inputfile = open('input', 'r')
+
+#count lines
+numlines = inputfile.readline()
+count = 0
+filecontent = [0]
+while numlines:
+    filecontent.append(numlines)
+    numlines = inputfile.readline()
+    count += 1
+print ("File has ", count, " lines")
