cf128e13b67e95e538d288e23187c2ca4edecc6d
MarkZ <surfsire@gmail.com>
2020-12-26 18:51:12 -0800

    Success! Part1

--- 2020/Day03/Day03.py
+++ 2020/Day03/Day03.py
@@ -13,3 +13,22 @@ while numlines:
     numlines = inputfile.readline()
     count += 1
 print ("File has ", count, " lines")
+
+x_coord = 1 #x starting point
+x_incr = 3  #x increment
+y_coord = 1 #y starting point
+y_incr = 1  #y increment
+
+tree_cnt = 0
+while (y_coord <= count):
+    entry_x = filecontent[y_coord]
+    if (x_coord > 31): x_coord -= 31                #Map/Pattern is 32 columns
+    if (entry_x[x_coord-1] == "#"): tree_cnt += 1   #increment tree count if # is found 
+    x_coord += x_incr
+    y_coord += y_incr
+
+print("There were", tree_cnt, "trees down the path")
+
+#close file, we're done with it
+print("...Closinging File")
+inputfile.close() 
