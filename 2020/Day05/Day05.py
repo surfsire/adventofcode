3d65cad4028b935d3412e95635a5c38b0fdab2d0
MarkZ <surfsire@gmail.com>
2020-12-30 19:24:12 -0800

    Day05-1 Success!

--- 2020/Day05/Day05.py
+++ 2020/Day05/Day05.py
@@ -14,13 +14,20 @@ while numlines:
     count += 1
 print ("File has ", count, " lines")
 
-i=1
+max_val = 0
+i = 1
 while (i <= count):
     entry_x = filecontent[i]
-
+    entry_x = entry_x[:entry_x.find("/n")]
+    entry_x = entry_x.replace("B","1")
+    entry_x = entry_x.replace("F","0")
+    entry_x = entry_x.replace("R","1")
+    entry_x = entry_x.replace("L","0")
+    print("Line ",i,"\t",filecontent[i],entry_x,"=",int(entry_x,2))
+    if (int(entry_x,2)) > (max_val): max_val = int(entry_x,2)
     i += 1
 
-print("Answer")
+print("Largest Value was:",max_val)
 
 
 #close file, we're done with it
