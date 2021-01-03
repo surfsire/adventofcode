8f2573b23bd28937c38cfde20937e355432f1545
MarkZ <surfsire@gmail.com>
2021-01-02 19:38:12 -0800

    Day07 - Initial

---
+++ 2020/Day07/Day07.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+#open file
+print("Opening File...")
+inputfile = open('input', 'r')
+
+#count lines
+numlines = inputfile.readline()
+count = 0
+filecontent = [0]
+while numlines:
+    filecontent.append(numlines)
+    numlines = inputfile.readline()
+    count += 1
+print ("File has ", count, " lines")
+
+i = 1
+while (i <= count):
+    entry_x = filecontent[i]
+
+    i += 1
+
+
+print("Answer:")
+
+
+#close file, we're done with it
+print("...Closing File")
+inputfile.close() 
