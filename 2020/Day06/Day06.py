ad0f3ed396b91d3e4318f3b5b8dcf0de6bfed8a1
MarkZ <surfsire@gmail.com>
2021-01-02 19:33:15 -0800

    Day06 -- Success!

--- 2020/Day06/Day06.py
+++ 2020/Day06/Day06.py
@@ -26,27 +26,42 @@ while (i <= count):
     i += 1
 groups.append(count+1)
 print("There are",group_cnt,"groups")
-print(groups)
 
 letters = "abcdefghijklmnopqrstuvwxyz"
 group_all = [0]
 i = 1
 while (i <= group_cnt):
-    temp_txt = filecontent[groups[i-1]:groups[i]]
-    temp_txt.pop(0)
-    print(temp_txt)
-    group_all.clear()
-    for w in temp_txt:
-        temp_string = w
-        temp_string = temp_string[:temp_string.index("\n")]
-        print(temp_string)
-        for x in temp_string:
-            group_all.append(x)
-    print(group_all)
-    for y in letters:
-        if (group_all.count(y) > 0): group_yes += 1
-    print(group_yes)
-    #print(filecontent[groups[0]:groups[1]])
+    temp_list = filecontent[groups[i-1]:groups[i]]
+    temp_list.pop(0)
+    temp_list_count = 0
+    for w in temp_list:
+        temp_list_count += 1
+    print("Group",i,"\t",temp_list_count,"Sets\t",temp_list)
+    letters = [0]
+    letters.clear()
+    for x in temp_list[0]:
+        if (x != "\n"): letters.append(x)
+    ii = 0
+    removal = [0]
+    removal.clear()
+    while (ii <= (temp_list_count-1)):
+        print("\r\n================\r\nChecking next set...",temp_list[ii])
+        for x in letters:
+            print("Checking for...",x,end=' ')
+            if (temp_list[ii].count(x) == 0):
+                print("\033[31mNot found!\033[0m\t",end=' ')
+                if (removal.count(x) == 0): removal.append(x)
+                print(removal)
+            else:
+                print("\033[34m Found!   \033[0m")
+        ii += 1
+    for y in removal:
+        letters.remove(y)
+    current_yeses = 0
+    for z in letters:
+        current_yeses += 1
+    group_yes += current_yeses
+    print("        Common",current_yeses,"\t",letters,"\r\n    Total now",group_yes,"\t=================================================================\r\n")
     i += 1
 
 
